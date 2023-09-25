567d28d7f517d8713dfe0c000ff99bd228eb1edb
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-09-26 00:20:35 +0530

    adding

--- HMS_FRONTEND/src/app/app.module.ts
+++ HMS_FRONTEND/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HmsHomeComponent } from './hms-home/hms-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HmsHomeComponent
   ],
   imports: [
     BrowserModule,
