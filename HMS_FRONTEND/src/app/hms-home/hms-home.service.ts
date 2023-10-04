a6e6f54833d40f79b0e95ca739c586097e2a8b8f
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-10-05 01:27:16 +0530

    home service ts

---
+++ HMS_FRONTEND/src/app/hms-home/hms-home.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import jwt_decode from 'jwt-decode';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HmsHomeService {
+
+  private apiUrl1 = 'http://localhost:8080/api/auth/authenticate';
+  private apiUrl2 = 'http://localhost:8080/api/auth/CurrentUser';
+  private apiUrl3 = 'http://localhost:8080/api/auth/UserProfile';
+  private apiUrl4 = 'http://localhost:8080/api/auth/UpdateProfile';
+
+  constructor(private http: HttpClient) { }
+
+  private getHeaders(): HttpHeaders {    //Class that can be use to get stored JWT 
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return headers;
+  }
+
+  login(index: string, password: string): Observable<{ token: string }> {
+    const body = {
+      index: index,
+      password: password
+    };
+    return this.http.post<{ token: string }>(`${this.apiUrl1}`, body);
+  }
+  
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  isLoggedIn() {
+    return localStorage.getItem('token') !== null;
+  }
+
+  getUsername(): Observable<string> { //Getting the Current Logged User
+    const headers = this.getHeaders();
+    return this.http.get<string>(`${this.apiUrl2}`,{ headers }); //Using stored token 
+  }
+
+  getUserProfile(): Observable<any> {
+    const headers = this.getHeaders();
+    return this.http.get<any>(`${this.apiUrl3}`, { headers });
+  }
+
+
+  getUserRole(): Observable<string> {
+    const token = localStorage.getItem('token') || ''; // Providing a default value of an empty string if token is null
+    const decodedToken: any = jwt_decode(token);
+    const role = decodedToken.role[0];
+    return of(role);
+  }
+  
+  updateUserProfile(updatedUser: any): Observable<any> {
+    const headers = this.getHeaders();
+    const id = updatedUser.id; // Assuming your updatedUser object contains the 'id' field
+    return this.http.put<any>(`${this.apiUrl4}`, updatedUser, { headers });
+  }
+
+
+}
