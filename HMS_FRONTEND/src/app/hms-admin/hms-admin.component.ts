dc0d77a812c93c80a230b477160229df72914564
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-10-04 23:35:31 +0530

    admin ts

---
+++ HMS_FRONTEND/src/app/hms-admin/hms-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hms-admin',
+  templateUrl: './hms-admin.component.html',
+  styleUrls: ['./hms-admin.component.css']
+})
+export class HmsAdminComponent {
+
+}
