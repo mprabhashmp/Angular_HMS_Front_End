5fdeafc7061cab8d9ee41dc1cbc4d685a1858ab2
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-10-05 01:27:02 +0530

    router module updated

--- HMS_FRONTEND/src/app/app-routing.module.ts
+++ HMS_FRONTEND/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGurdService } from './auth-gurd.service';
+import { HmsHomeComponent } from './hms-home/hms-home.component';
+import { RouterresolverService } from './routerresolver.service';
+import { HmsAdminComponent } from './hms-admin/hms-admin.component';
+import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HmsHomeComponent},
+  {path:"hms-admin",component:HmsAdminComponent, canActivate: [AuthGurdService], data: { allowedRoles: ['ADMIN'] },resolve: { userRole: RouterresolverService }},
+  {path:"unauthorized",component:UnauthorizedComponent,canActivate: [AuthGurdService], data: { allowedRoles: ['ADMIN'] },resolve: { userRole: RouterresolverService }}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
