6061de187b2832cfc680892e006225d2910ec5ff
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-10-30 18:16:53 +0530

    create register component

---
+++ HMS_FRONTEND/src/app/registration/registration.service.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  private apiUrl = 'http://localhost:8080/api/auth/register';
+
+  registerUser(regData : any){
+    // Send the registration request to the server
+    const url = `${this.apiUrl}`
+    return this.http.post(url , regData);
+  }
+}
