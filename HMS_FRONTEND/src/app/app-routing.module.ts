5c918177a7b0a306e80e7d74d2ad147590ba5ccb
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-10-30 18:15:20 +0530

    update modules

--- HMS_FRONTEND/src/app/app-routing.module.ts
+++ HMS_FRONTEND/src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { HmsHomeComponent } from './hms-home/hms-home.component';
 import { RouterresolverService } from './routerresolver.service';
 import { HmsAdminComponent } from './hms-admin/hms-admin.component';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { StaffRegisterationComponent } from './staff-registeration/staff-registeration.component';
 
 const routes: Routes = [
   {path:"",component:HmsHomeComponent},
   {path:"hms-admin",component:HmsAdminComponent, canActivate: [AuthGurdService], data: { allowedRoles: ['ADMIN'] },resolve: { userRole: RouterresolverService }},
-  {path:"unauthorized",component:UnauthorizedComponent,canActivate: [AuthGurdService], data: { allowedRoles: ['ADMIN'] },resolve: { userRole: RouterresolverService }}
-
+  {path:"unauthorized",component:UnauthorizedComponent,canActivate: [AuthGurdService], data: { allowedRoles: ['ADMIN'] },resolve: { userRole: RouterresolverService }},
+  {path:"registration",component:RegistrationComponent,canActivate: [AuthGurdService], data: { allowedRoles: ['ADMIN'] },resolve: { userRole: RouterresolverService }},
+  {path:"staff-registeration",component:StaffRegisterationComponent,canActivate: [AuthGurdService], data: { allowedRoles: ['ADMIN'] },resolve: { userRole: RouterresolverService }}
 ];
 
 @NgModule({
