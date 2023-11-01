0882cac1330ec67e328b4bca69744093bfc9979b
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-11-01 18:06:01 +0530

    create dean dashboard
    Co-Authored-By: Hansini Rathnayaka <104292861+Hansini98-R@users.noreply.github.com>

---
+++ HMS_FRONTEND/src/app/hms-dean/hms-dean.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hms-dean',
+  templateUrl: './hms-dean.component.html',
+  styleUrls: ['./hms-dean.component.css']
+})
+export class HmsDeanComponent {
+
+}
