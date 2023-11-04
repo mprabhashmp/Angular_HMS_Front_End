97e1897c45fd1275183a49b85a9d34fd0ddb728a
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-11-04 20:31:42 +0530

    warden  update
    Co-Authored-By: Hansini Rathnayaka <104292861+Hansini98-R@users.noreply.github.com>
    Co-Authored-By: Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
    Co-Authored-By: Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>

--- HMS_FRONTEND/src/app/hms-warden/hms-warden.component.ts
+++ HMS_FRONTEND/src/app/hms-warden/hms-warden.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { WardenServiceService } from './warden-service.service';
 
 @Component({
@@ -6,25 +6,39 @@ import { WardenServiceService } from './warden-service.service';
   templateUrl: './hms-warden.component.html',
   styleUrls: ['./hms-warden.component.css']
 })
-export class HmsWardenComponent {
+export class HmsWardenComponent{
 
 
-  currentUser: any; // Adjust the type based on your user object structure
+p: number = 1;
+complains:any[]=[];
+ 
 
-  constructor(private userService: WardenServiceService) {}
+constructor(private wardenservice: WardenServiceService) { }
 
-  // ngOnInit(): void {
-  //   this.getCurrentUser();
-  // }
+  ngOnInit() {
 
-  // getCurrentUser(): void {
-  //   this.userService.getCurrentUser().subscribe(
-  //     (user: any) => {
-  //       this.currentUser = user;
-  //     },
-  //     (error: any) => {
-  //       console.log('Error retrieving current user:', error);
-  //     }
-  //   );
-  // }
+    this.getWardenComplains();
+
+  }
+  
+  formatDate(timestamp: string): string {
+    const date = new Date(timestamp);
+    return date.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' }) + '| ' + date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+  }
+
+  getWardenComplains(): void {
+    this.wardenservice.getWardenComplains().subscribe(complains => {
+      this.complains = complains;
+    });
+  }
+
+  
+  resolveComplain(c_id: number): void {
+    if (window.confirm('Are you sure you want to Accept this complaint?')) {
+      this.wardenservice.resolveComplain(c_id).subscribe(() => {
+        this.getWardenComplains();
+      });
+    }
+  }
+  
 }
