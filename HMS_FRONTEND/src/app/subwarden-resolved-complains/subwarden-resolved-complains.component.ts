72627d38a342cec2b415ac6932b0c4fcbb740c23
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-11-04 20:34:04 +0530

    subwarden resolved
    Co-Authored-By: Hansini Rathnayaka <104292861+Hansini98-R@users.noreply.github.com>
    Co-Authored-By: Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
    Co-Authored-By: Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>

---
+++ HMS_FRONTEND/src/app/subwarden-resolved-complains/subwarden-resolved-complains.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subwarden-resolved-complains',
+  templateUrl: './subwarden-resolved-complains.component.html',
+  styleUrls: ['./subwarden-resolved-complains.component.css']
+})
+export class SubwardenResolvedComplainsComponent {
+
+}
