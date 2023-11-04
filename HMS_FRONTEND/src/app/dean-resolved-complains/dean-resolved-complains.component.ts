1442fe79fface908c1c4632b6c9807072e46d278
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-11-05 01:48:34 +0530

    created
    Co-Authored-By: Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
    Co-Authored-By: Hansini Rathnayaka <104292861+Hansini98-R@users.noreply.github.com>
    Co-Authored-By: Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>

---
+++ HMS_FRONTEND/src/app/dean-resolved-complains/dean-resolved-complains.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dean-resolved-complains',
+  templateUrl: './dean-resolved-complains.component.html',
+  styleUrls: ['./dean-resolved-complains.component.css']
+})
+export class DeanResolvedComplainsComponent {
+
+}
