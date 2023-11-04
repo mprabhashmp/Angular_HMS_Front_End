c91b5092d0c9b99684158a0f7ed382431449bc79
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-11-04 20:33:08 +0530

    student resolved
    Co-Authored-By: Hansini Rathnayaka <104292861+Hansini98-R@users.noreply.github.com>
    Co-Authored-By: Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
    Co-Authored-By: Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>

---
+++ HMS_FRONTEND/src/app/student-resolved-complains/student-resolved-complains.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-resolved-complains',
+  templateUrl: './student-resolved-complains.component.html',
+  styleUrls: ['./student-resolved-complains.component.css']
+})
+export class StudentResolvedComplainsComponent {
+
+}
