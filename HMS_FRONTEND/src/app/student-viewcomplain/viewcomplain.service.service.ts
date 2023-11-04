687dfdca6137e04e7c564ba4d9ee8337aac83d72
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-11-04 20:33:35 +0530

    view component update
    Co-Authored-By: Hansini Rathnayaka <104292861+Hansini98-R@users.noreply.github.com>
    Co-Authored-By: Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
    Co-Authored-By: Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>

--- HMS_FRONTEND/src/app/student-viewcomplain/viewcomplain.service.service.ts
+++ HMS_FRONTEND/src/app/student-viewcomplain/viewcomplain.service.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class ViewcomplainServiceService {
 
   private apiUrl= 'http://localhost:8080/api/user/complain/getComplainsByCurrentUser';
-  private complainCount= 'http://localhost:8080/api/admin';
+  // private complainCount= 'http://localhost:8080/api/admin';
 
   constructor(private http: HttpClient) { }
 
@@ -19,8 +19,8 @@ getComplains(): Observable<any[]> {
 }
 
 
-getcomplainCount(): Observable<number> {
-  return this.http.get<number>(`${this.complainCount}/userCount`);
-}
+// getcomplainCount(): Observable<number> {
+//   return this.http.get<number>(`${this.complainCount}/userCount`);
+// }
 
 }
