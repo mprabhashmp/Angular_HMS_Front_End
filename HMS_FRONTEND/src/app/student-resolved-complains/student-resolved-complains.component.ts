01b1dc143f461949acd8fb72a8d89081a7ac0f2f
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-11-05 01:49:08 +0530

    updates

--- HMS_FRONTEND/src/app/student-resolved-complains/student-resolved-complains.component.ts
+++ HMS_FRONTEND/src/app/student-resolved-complains/student-resolved-complains.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StudentResolveServiceService } from './student-resolve-service.service';
+import { HmsHomeService } from '../hms-home/hms-home.service';
 
 @Component({
   selector: 'app-student-resolved-complains',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class StudentResolvedComplainsComponent {
 
+  p: number = 1;
+  complains:any[]=[];
+   
+  
+  constructor(private hmsservice: HmsHomeService) { }
+  
+    ngOnInit() {
+  
+      this.getResolveComplains();
+  
+  
+    }
+    
+    formatDate(timestamp: string): string {
+      const date = new Date(timestamp);
+      return date.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' }) + '| ' + date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+    }
+
+    getResolveComplains(): void {
+      this.hmsservice.getResolveComplains().subscribe(complains => {
+        this.complains = complains;
+      });
+    }
 }
