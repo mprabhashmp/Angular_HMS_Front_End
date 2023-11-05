9c64596fbc5ff3e6992b75ff2814606935031080
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-11-05 13:39:58 +0530

    st view warden
    Co-Authored-By: Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
    Co-Authored-By: Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
    Co-Authored-By: Hansini Rathnayaka <104292861+Hansini98-R@users.noreply.github.com>

---
+++ HMS_FRONTEND/src/app/student-view-warden/student-view-warden.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentViewWardenService {
+
+  
+//PASSED FROM HMS HOME SERVICE
+}
